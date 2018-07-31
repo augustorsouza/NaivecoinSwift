066f19b512bbcc2ed768b5668c0c927ff835bf92
Augusto Souza <augustorsouza@gmail.com>
2018-07-31 18:01:27 -0300

    Refactor

--- Sources/App/Models/Blockchain.swift
+++ Sources/App/Models/Blockchain.swift
@@ -73,27 +73,3 @@ final class Blockchain {
         }
     }
 }
-
-protocol BlockValidator {
-    func isValid(block: Block, previousBlock: Block) -> Bool
-}
-
-struct IndexBlockValidator: BlockValidator {
-    func isValid(block: Block, previousBlock: Block) -> Bool {
-        return block.index - 1 == previousBlock.index
-    }
-}
-
-struct PreviousHashBlockValidator: BlockValidator {
-    func isValid(block: Block, previousBlock: Block) -> Bool {
-        return block.previousHash == previousBlock.hash
-    }
-}
-
-struct HashIntegrityBlockValidator: BlockValidator {
-    func isValid(block: Block, previousBlock: Block) -> Bool {
-        guard let calculatedHash = try? Blockchain.shared.calculateHash(index: block.index, previousHash: block.previousHash, data: block.data, timestamp: block.timestamp)
-            else { return false }
-        return calculatedHash == block.hash
-    }
-}
